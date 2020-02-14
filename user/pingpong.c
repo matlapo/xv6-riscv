4abe00fa7a91d50aaead37184fa6c351ade57fda
Mathieu Lapointe <mathieu.lapointe3@mail.mcgill.ca>
2020-02-14 15:44:21 -0500

    clean up

--- user/pingpong.c
+++ user/pingpong.c
@@ -2,11 +2,6 @@
 #include "kernel/stat.h"
 #include "user/user.h"
 
-
-/*int pipe(int*);*/
-/*int write(int, const void*, int);*/
-/*int read(int, void*, int);*/
-
 int main(int argc, char *argv[])
 {
     int child_pid;
@@ -28,22 +23,20 @@ int main(int argc, char *argv[])
     if (child_pid == 0) {
         int nbytes = read(parent_fd[0], &buffer, sizeof(buffer));
         if (nbytes > 0) {
-            printf("child(%d) received %d\n", getpid(), buffer[0]);
+            printf("%d: received ping\n", getpid(), buffer[0]);
 
             // send byte back
             write(child_fd[1], &byte, sizeof(byte));
-            printf("child(%d) sent %d\n", getpid(), byte);
         }
     }
     // parent process
     else {
         write(parent_fd[1], &byte, sizeof(byte));
-        printf("parent(%d) sent %d\n", getpid(), byte);
 
         // read back the byte sent
         int nbytes = read(child_fd[0], &buffer, sizeof(buffer));
         if (nbytes > 0) {
-            printf("parent(%d) received %d\n", getpid(), buffer[0]);
+            printf("%d: received pong\n", getpid(), buffer[0]);
         }
     }
     exit(0);
